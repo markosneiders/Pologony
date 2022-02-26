508df99552b2ffd91dc9824ddce422a83c42eade
Zappingmadnnes <roberts.mezals@gmail.com>
2022-02-26 14:01:47 +0200

    Smooth jazz movement controler

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
-import { Vector3 } from "three";
-
-// Debug
+import { PlaneGeometry, Vector3 } from "three";
 
 //Ray
 const raycaster = new THREE.Raycaster();
@@ -85,8 +83,9 @@ camera.position.z = 2;
 scene.add(camera);
 
 // Controls
-//const controls = new OrbitControls(camera, canvas);
-//controls.enableDamping = true;
+var pressedKeys = {};
+pressedKeys[68] = false;
+pressedKeys[65] = false;
 var xSpeed = 0.1; //speed at which player moves
 var maxThreshold = 20; // how far right the player can go
 var minThreshold = -20; // how far left the player can go
@@ -94,12 +93,11 @@ var maxRThreshold = 0.3; // how far counter clockwise the player can rotate
 var minRThreshold = -0.3; // how far clockwise the player can rotate
 var rSpeed = 0.02; // player rotation speed
 var rReturnSpeed = 0.05; // player rotation return speed
+var canShoot = 0;
+var shootDelay = 1;
 
-var rMove = 0;
-var xMove = 0;
 document.addEventListener("keydown", onDocumentKeyDown, false);
 document.addEventListener("keyup", onDocumentKeyUp, false);
-
 /**
  * Renderer
  */
@@ -110,13 +108,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Animate
- */
-
-const clock = new THREE.Clock();
-
-//GUI stuff
+//GUI debug stuff
 const gui = new dat.GUI();
 const cameraFolder = gui.addFolder("Camera");
 cameraFolder.add(camera.position, "z").min(0).max(30).step(1);
@@ -124,76 +116,31 @@ cameraFolder.add(camera.position, "y").min(0).max(20).step(1);
 cameraFolder.add(enemy.position, "x").min(-10).max(10).step(0.1);
 
 const tick = () => {
-	//const elapsedTime = clock.getElapsedTime();
-	player.position.x += xMove;
+	//player side movement
+	if (player.position.x < maxThreshold && pressedKeys[68] == true) {
+		player.position.x += xSpeed;
+	}
+	if (player.position.x > minThreshold && pressedKeys[65] == true) {
+		player.position.x -= xSpeed;
+	}
 
-	if (player.rotation.z > maxRThreshold) {
-		player.rotation.z -= rSpeed;
-	} else if (player.rotation.z < minRThreshold) {
+	//player rotation
+	if (player.rotation.z < maxRThreshold && pressedKeys[65] == true) {
 		player.rotation.z += rSpeed;
 	}
-	if (player.rotation.z > 0 && rMove == 0) {
-		player.rotation.z -= rReturnSpeed;
-	} else if (player.rotation.z < 0 && rMove == 0) {
-		player.rotation.z += rReturnSpeed;
+	if (player.rotation.z > minRThreshold && pressedKeys[68] == true) {
+		player.rotation.z -= rSpeed;
 	}
-	if (player.rotation.z < 1 && player.rotation.z > -1) {
-		player.rotation.z += rMove;
-	}
-
-	// go through bullets array and update position
-	// remove bullets when appropriate
-	for (var index = 0; index < bullets.length; index += 1) {
-		if (bullets[index] === undefined) continue;
-		if (bullets[index].alive == false) {
-			bullets.splice(index, 1);
-			continue;
+	if (pressedKeys[68] == false && pressedKeys[65] == false) {
+		//player rotation reset
+		if (player.rotation.z > 0) {
+			player.rotation.z -= rReturnSpeed;
 		}
-		var ray = new THREE.Raycaster();
-		raycaster.set(
-			new Vector3(bullets[index].position.x, 0, bullets[index].position.z),
-			new Vector3(0, 0, -1)
-		);
-		const intersects = raycaster.intersectObjects(scene.children);
-		try {
-			console.log(intersects[0].distance);
-		} catch (err) {
-			null;
+		if (player.rotation.z < 0) {
+			player.rotation.z += rReturnSpeed;
 		}
-		bullets[index].position.add(bullets[index].velocity);
 	}
-
-	//camera follow
-	camera.position.x = player.position.x;
-
-	//raycasting
-	// raycaster.set(new Vector3(player.position.x, 0, 0), new Vector3(0, 0, -1));
-
-	// const intersects = raycaster.intersectObjects(scene.children);
-	// try {
-	// 	console.log(intersects[0].distance);
-	// } catch (err) {
-	// 	null;
-	// }
-
-	// Render
-	renderer.render(scene, camera);
-
-	// Call tick again on the next frame
-	window.requestAnimationFrame(tick);
-};
-
-function onDocumentKeyDown(event) {
-	var keyCode = event.which;
-
-	//Movement ifs
-	if (keyCode == 65 && player.position.x > minThreshold) {
-		xMove = -xSpeed;
-	} else if (keyCode == 68 && player.position.x < maxThreshold) {
-		xMove = +xSpeed;
-	} else if (keyCode == 32) {
-		console.log("Shoot");
-
+	if (pressedKeys[32] == true && canShoot == 0) {
 		//space pressed
 
 		// creates a bullet as a Mesh object
@@ -222,28 +169,51 @@ function onDocumentKeyDown(event) {
 		// add to scene, array, and set the delay to 10 frames
 		bullets.push(bullet);
 		scene.add(bullet);
-	} else {
-		xMove = 0;
+		canShoot = 10;
 	}
 
-	if (keyCode == 65) {
-		rMove = rSpeed;
-	} else if (keyCode == 68) {
-		rMove = -rSpeed;
+	// go through bullets array and update position
+	// remove bullets when appropriate
+	for (var index = 0; index < bullets.length; index += 1) {
+		if (bullets[index] === undefined) continue;
+		if (bullets[index].alive == false) {
+			bullets.splice(index, 1);
+			continue;
+		}
+		var ray = new THREE.Raycaster();
+		raycaster.set(
+			new Vector3(bullets[index].position.x, 0, bullets[index].position.z),
+			new Vector3(0, 0, -1)
+		);
+		const intersects = raycaster.intersectObjects(scene.children);
+		try {
+			console.log(intersects[0].distance);
+		} catch (err) {
+			null;
+		}
+		bullets[index].position.add(bullets[index].velocity);
 	}
+	//shoot delay
+	if (canShoot > 0) canShoot -= shootDelay;
+
+	//camera follow
+	camera.position.x = player.position.x;
+
+	// Render
+	renderer.render(scene, camera);
+
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick);
+};
+
+//updates key pressed array
+function onDocumentKeyDown(event) {
+	var keyCode = event.which;
+	pressedKeys[keyCode] = true;
 }
 function onDocumentKeyUp(event) {
 	var keyCode = event.which;
-	if (keyCode == 65 && player.position.x > minThreshold) {
-		xMove = 0;
-	} else if (keyCode == 68 && player.position.x < maxThreshold) {
-		xMove = 0;
-	}
-	if (keyCode == 65) {
-		rMove = 0;
-	} else if (keyCode == 68) {
-		rMove = 0;
-	}
+	pressedKeys[keyCode] = false;
 }
 
 tick();
