f6640e43a9a7127b40f916e61a03dc841334f6be
markosneiders <67235193+markosneiders@users.noreply.github.com>
2022-02-26 15:37:43 +0200

    Enemy destruction (Roberts izdarija)

--- src/script.js
+++ src/script.js
@@ -25,6 +25,7 @@ bulletMaterial.color = new THREE.Color(0xff0000);
 
 const enemyMaterial = new THREE.MeshBasicMaterial();
 enemyMaterial.color = new THREE.Color(0x0000ff);
+enemyMaterial.side = THREE.DoubleSide;
 
 // Mesh
 const player = new THREE.Mesh(geometry, material);
@@ -32,10 +33,11 @@ scene.add(player);
 
 const enemy = new THREE.Mesh(geometry, enemyMaterial);
 scene.add(enemy);
-enemy.position.z = -30;
+enemy.position.z = -10;
 enemy.position.x = 3;
 
 var bullets = [];
+var bulletLifetime = 10000;
 
 // Lights
 
@@ -49,22 +51,22 @@ scene.add(pointLight);
  * Sizes
  */
 const sizes = {
-	width: window.innerWidth,
-	height: window.innerHeight,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 window.addEventListener("resize", () => {
-	// Update sizes
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-	// Update camera
-	camera.aspect = sizes.width / sizes.height;
-	camera.updateProjectionMatrix();
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-	// Update renderer
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 /**
@@ -72,10 +74,10 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-	90,
-	sizes.width / sizes.height,
-	0.1,
-	1000
+  90,
+  sizes.width / sizes.height,
+  0.1,
+  1000
 );
 camera.position.x = 0;
 camera.position.y = 2;
@@ -102,8 +104,8 @@ document.addEventListener("keyup", onDocumentKeyUp, false);
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-	canvas: canvas,
-	alpha: true,
+  canvas: canvas,
+  alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -116,104 +118,108 @@ cameraFolder.add(camera.position, "y").min(0).max(20).step(1);
 cameraFolder.add(enemy.position, "x").min(-10).max(10).step(0.1);
 
 const tick = () => {
-	//player side movement
-	if (player.position.x < maxThreshold && pressedKeys[68] == true) {
-		player.position.x += xSpeed;
-	}
-	if (player.position.x > minThreshold && pressedKeys[65] == true) {
-		player.position.x -= xSpeed;
-	}
+  //player side movement
+  if (player.position.x < maxThreshold && pressedKeys[68] == true) {
+    player.position.x += xSpeed;
+  }
+  if (player.position.x > minThreshold && pressedKeys[65] == true) {
+    player.position.x -= xSpeed;
+  }
 
-	//player rotation
-	if (player.rotation.z < maxRThreshold && pressedKeys[65] == true) {
-		player.rotation.z += rSpeed;
-	}
-	if (player.rotation.z > minRThreshold && pressedKeys[68] == true) {
-		player.rotation.z -= rSpeed;
-	}
-	if (pressedKeys[68] == false && pressedKeys[65] == false) {
-		//player rotation reset
-		if (player.rotation.z > 0) {
-			player.rotation.z -= rReturnSpeed;
-		}
-		if (player.rotation.z < 0) {
-			player.rotation.z += rReturnSpeed;
-		}
-	}
-	if (pressedKeys[32] == true && canShoot == 0) {
-		//space pressed
+  //player rotation
+  if (player.rotation.z < maxRThreshold && pressedKeys[65] == true) {
+    player.rotation.z += rSpeed;
+  }
+  if (player.rotation.z > minRThreshold && pressedKeys[68] == true) {
+    player.rotation.z -= rSpeed;
+  }
+  if (pressedKeys[68] == false && pressedKeys[65] == false) {
+    //player rotation reset
+    if (player.rotation.z > 0) {
+      player.rotation.z -= rReturnSpeed;
+    }
+    if (player.rotation.z < 0) {
+      player.rotation.z += rReturnSpeed;
+    }
+  }
+  if (pressedKeys[32] == true && canShoot == 0) {
+    //space pressed
 
-		// creates a bullet as a Mesh object
-		var bullet = new THREE.Mesh(
-			new THREE.SphereGeometry(0.05, 8, 8),
-			new THREE.MeshBasicMaterial({ color: 0xffffff })
-		);
+    // creates a bullet as a Mesh object
+    var bullet = new THREE.Mesh(
+      new THREE.SphereGeometry(0.05, 8, 8),
+      new THREE.MeshBasicMaterial({ color: 0xffffff })
+    );
 
-		// position the bullet to come from the player's weapon
-		bullet.position.set(player.position.x, 0, 0);
+    // position the bullet to come from the player's weapon
+    bullet.position.set(player.position.x, 0, 0);
 
-		// set the velocity of the bullet
-		bullet.velocity = new THREE.Vector3(0, 0, -0.1);
+    // set the velocity of the bullet
+    bullet.velocity = new THREE.Vector3(0, 0, -0.1);
 
-		bullet.raycast;
+    bullet.raycast;
 
-		// after 1000ms, set alive to false and remove from scene
-		// setting alive to false flags our update code to remove
-		// the bullet from the bullets array
-		bullet.alive = true;
-		setTimeout(function () {
-			bullet.alive = false;
-			scene.remove(bullet);
-		}, 10000);
+    // after 1000ms, set alive to false and remove from scene
+    // setting alive to false flags our update code to remove
+    // the bullet from the bullets array
+    bullet.alive = true;
+    setTimeout(function () {
+      bullet.alive = false;
+      scene.remove(bullet);
+    }, bulletLifetime);
 
-		// add to scene, array, and set the delay to 10 frames
-		bullets.push(bullet);
-		scene.add(bullet);
-		canShoot = 10;
-	}
+    // add to scene, array, and set the delay to 10 frames
+    bullets.push(bullet);
+    scene.add(bullet);
+    canShoot = 10;
+  }
 
-	// go through bullets array and update position
-	// remove bullets when appropriate
-	for (var index = 0; index < bullets.length; index += 1) {
-		if (bullets[index] === undefined) continue;
-		if (bullets[index].alive == false) {
-			bullets.splice(index, 1);
-			continue;
-		}
-		var ray = new THREE.Raycaster();
-		raycaster.set(
-			new Vector3(bullets[index].position.x, 0, bullets[index].position.z),
-			new Vector3(0, 0, -1)
-		);
-		const intersects = raycaster.intersectObjects(scene.children);
-		try {
-			console.log(intersects[0].distance);
-		} catch (err) {
-			null;
-		}
-		bullets[index].position.add(bullets[index].velocity);
-	}
-	//shoot delay
-	if (canShoot > 0) canShoot -= shootDelay;
+  // go through bullets array and update position
+  // remove bullets when appropriate
+  for (var index = 0; index < bullets.length; index += 1) {
+    if (bullets[index] === undefined) continue;
+    if (bullets[index].alive == false) {
+      bullets.splice(index, 1);
+      continue;
+    }
+    var ray = new THREE.Raycaster();
+    raycaster.set(
+      new Vector3(bullets[index].position.x, 0, bullets[index].position.z),
+      new Vector3(0, 0, -1)
+    );
+    const intersects = raycaster.intersectObjects(scene.children);
+    try {
+      if (intersects[0].distance < 0.05) {
+        //if raycast distance is smaller than bullet radius
+        scene.remove(intersects[0].object);
+        scene.remove(bullets[index]);
+      }
+    } catch (err) {
+      null;
+    }
+    bullets[index].position.add(bullets[index].velocity);
+  }
+  //shoot delay
+  if (canShoot > 0) canShoot -= shootDelay;
 
-	//camera follow
-	camera.position.x = player.position.x;
+  //camera follow
+  camera.position.x = player.position.x;
 
-	// Render
-	renderer.render(scene, camera);
+  // Render
+  renderer.render(scene, camera);
 
-	// Call tick again on the next frame
-	window.requestAnimationFrame(tick);
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
 };
 
 //updates key pressed array
 function onDocumentKeyDown(event) {
-	var keyCode = event.which;
-	pressedKeys[keyCode] = true;
+  var keyCode = event.which;
+  pressedKeys[keyCode] = true;
 }
 function onDocumentKeyUp(event) {
-	var keyCode = event.which;
-	pressedKeys[keyCode] = false;
+  var keyCode = event.which;
+  pressedKeys[keyCode] = false;
 }
 
 tick();
