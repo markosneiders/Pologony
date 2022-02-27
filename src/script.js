2b744c1ac69f0bf0d356a0c0119f51d13c821017
markosneiders <67235193+markosneiders@users.noreply.github.com>
2022-02-27 02:08:35 +0200

    Smooooooooooth camera work

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
 import * as dat from "dat.gui";
+import * as TWEEN from "@tweenjs/tween.js";
 import { Vector3 } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
@@ -23,24 +24,24 @@ const loader = new OBJLoader();
 const mtlLoader = new MTLLoader();
 
 mtlLoader.load("models/craft_speederA.mtl", function (materials) {
-	materials.preload();
+    materials.preload();
 
-	const objLoader = new OBJLoader();
-	objLoader.setMaterials(materials);
-	objLoader.load(
-		"models/craft_speederA.obj",
-		function (object) {
-			player = object;
-			player.rotation.y = Math.PI;
-			scene.add(player);
-		},
-		function (xhr) {
-			console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-		},
-		function (error) {
-			console.log("An error happened");
-		}
-	);
+    const objLoader = new OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.load(
+        "models/craft_speederA.obj",
+        function (object) {
+            player = object;
+            player.rotation.y = Math.PI;
+            scene.add(player);
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
+        function (error) {
+            console.log("An error happened");
+        }
+    );
 });
 
 // Objects
@@ -78,22 +79,22 @@ scene.add(ambientLight);
  * Sizes
  */
 const sizes = {
-	width: window.innerWidth,
-	height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
 window.addEventListener("resize", () => {
-	// Update sizes
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-	// Update camera
-	camera.aspect = sizes.width / sizes.height;
-	camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
-	// Update renderer
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 /**
@@ -101,15 +102,18 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-	90,
-	sizes.width / sizes.height,
-	0.1,
-	1000
+    90,
+    sizes.width / sizes.height,
+    0.1,
+    1000
 );
 camera.position.x = 0;
-camera.position.y = 2;
-camera.position.z = 2;
+camera.position.y = 25;
+camera.position.z = 30;
+camera.rotation.x = -1.14;
 scene.add(camera);
+var cameraPositionTarget = new THREE.Vector3(0, 3, 5); // create on init
+var cameraRotationTarget = new THREE.Vector3(0, 0, 0); // create on init
 
 //Audio
 const listener = new THREE.AudioListener();
@@ -145,8 +149,8 @@ document.addEventListener("keyup", onDocumentKeyUp, false);
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-	canvas: canvas,
-	alpha: true,
+    canvas: canvas,
+    alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -154,218 +158,274 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 //GUI debug stuff
 const gui = new dat.GUI();
 const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, "z").min(0).max(30).step(1);
-cameraFolder.add(camera.position, "y").min(0).max(20).step(1);
+cameraFolder.add(camera.position, "x").min(-20).max(20).step(1);
+cameraFolder.add(camera.position, "y").min(-5).max(40).step(1);
+cameraFolder.add(camera.position, "z").min(0).max(40).step(1);
+cameraFolder.add(camera.rotation, "x").min(-2).max(2).step(0.01);
 
 const tick = () => {
-	//player side movement
-	if (player.position.x < maxThreshold && pressedKeys[68] == true) {
-		player.position.x += xSpeed;
-	}
-	if (player.position.x > minThreshold && pressedKeys[65] == true) {
-		player.position.x -= xSpeed;
-	}
+    TWEEN.update();
+    //player side movement
+    if (player.position.x < maxThreshold && pressedKeys[68] == true) {
+        player.position.x += xSpeed;
+    }
+    if (player.position.x > minThreshold && pressedKeys[65] == true) {
+        player.position.x -= xSpeed;
+    }
 
-	//player rotation
-	if (player.rotation.z > minRThreshold && pressedKeys[65] == true) {
-		player.rotation.z -= rSpeed;
-	}
-	if (player.rotation.z < maxRThreshold && pressedKeys[68] == true) {
-		player.rotation.z += rSpeed;
-	}
-	if (pressedKeys[68] == false && pressedKeys[65] == false) {
-		//player rotation reset
-		if (player.rotation.z > 0) {
-			player.rotation.z -= rReturnSpeed;
-		}
-		if (player.rotation.z < 0) {
-			player.rotation.z += rReturnSpeed;
-		}
-	}
-	if (pressedKeys[32] == true && canShoot == 0) {
-		//space pressed
-		audioLoader.load("sounds/laserSmall_000.ogg", function (buffer) {
-			const shootSound = new THREE.Audio(listener);
-			shootSound.setBuffer(buffer);
-			shootSound.setVolume(0.5);
-			shootSound.play(); //play shoot sound
-		});
-		// creates a bullet as a Mesh object
-		var bullet = new THREE.Mesh(
-			new THREE.BoxGeometry(0.1, 0.1, 1),
-			new THREE.MeshBasicMaterial({ color: 0xff0000 })
-		);
+    //player rotation
+    if (player.rotation.z > minRThreshold && pressedKeys[65] == true) {
+        player.rotation.z -= rSpeed;
+    }
+    if (player.rotation.z < maxRThreshold && pressedKeys[68] == true) {
+        player.rotation.z += rSpeed;
+    }
+    if (pressedKeys[68] == false && pressedKeys[65] == false) {
+        //player rotation reset
+        if (player.rotation.z > 0) {
+            player.rotation.z -= rReturnSpeed;
+        }
+        if (player.rotation.z < 0) {
+            player.rotation.z += rReturnSpeed;
+        }
+        if (player.rotation.z < 0.05 && player.rotation.z > -0.05) {
+            player.rotation.z = 0;
+        }
+    }
+    if (pressedKeys[32] == true && canShoot == 0) {
+        //space pressed
+        audioLoader.load("sounds/laserSmall_000.ogg", function (buffer) {
+            const shootSound = new THREE.Audio(listener);
+            shootSound.setBuffer(buffer);
+            shootSound.setVolume(0.5);
+            shootSound.play(); //play shoot sound
+        });
+        // creates a bullet as a Mesh object
+        var bullet = new THREE.Mesh(
+            new THREE.BoxGeometry(0.1, 0.1, 1),
+            new THREE.MeshBasicMaterial({ color: 0xff0000 })
+        );
 
-		// position the bullet to come from the player's weapon
-		bullet.position.set(player.position.x, 0, 0);
+        // position the bullet to come from the player's weapon
+        bullet.position.set(player.position.x, 0, 0);
 
-		// set the velocity of the bullet
-		bullet.velocity = new THREE.Vector3(0, 0, -bulletVelocity);
+        // set the velocity of the bullet
+        bullet.velocity = new THREE.Vector3(0, 0, -bulletVelocity);
 
-		bullet.raycast;
+        bullet.raycast;
 
-		bullet.alive = true;
-		setTimeout(function () {
-			bullet.alive = false;
-			scene.remove(bullet);
-		}, bulletLifetime);
+        bullet.alive = true;
+        setTimeout(function () {
+            bullet.alive = false;
+            scene.remove(bullet);
+        }, bulletLifetime);
 
-		// add to scene, array, and set the delay to 10 frames
-		bullets.push(bullet);
-		scene.add(bullet);
+        // add to scene, array, and set the delay to 10 frames
+        bullets.push(bullet);
+        scene.add(bullet);
 
-		canShoot = 10;
-	}
+        canShoot = 10;
+    }
 
-	// go through bullets array and update position
-	// remove bullets when appropriate
-	for (var index = 0; index < bullets.length; index += 1) {
-		if (bullets[index] === undefined) continue;
-		if (bullets[index].alive == false) {
-			bullets.splice(index, 1);
-			continue;
-		}
-		var ray = new THREE.Raycaster();
-		raycaster.set(
-			new Vector3(bullets[index].position.x, 0, bullets[index].position.z),
-			new Vector3(0, 0, -1)
-		);
-		const intersects = raycaster.intersectObjects(scene.children, true);
+    // go through bullets array and update position
+    // remove bullets when appropriate
+    for (var index = 0; index < bullets.length; index += 1) {
+        if (bullets[index] === undefined) continue;
+        if (bullets[index].alive == false) {
+            bullets.splice(index, 1);
+            continue;
+        }
+        var ray = new THREE.Raycaster();
+        raycaster.set(
+            new Vector3(
+                bullets[index].position.x,
+                0,
+                bullets[index].position.z
+            ),
+            new Vector3(0, 0, -1)
+        );
+        const intersects = raycaster.intersectObjects(scene.children, true);
 
-		try {
-			if (intersects[0].distance < 0.05) {
-				//if raycast distance is smaller than bullet radius
-				score += 1;
-				const killSound = intersects[0].object.parent.children[2];
-				killSound.play();
-				scene.remove(intersects[0].object.parent);
-				bullets[index].alive = false;
-				scene.remove(bullets[index]);
-			}
-		} catch (err) {
-			null;
-		}
-		bullets[index].position.add(bullets[index].velocity);
-	}
-	//shoot delay
-	if (canShoot > 0) canShoot -= shootDelay;
-	if (canShoot < 0) canShoot = 0;
+        try {
+            if (intersects[0].distance < 0.05) {
+                //if raycast distance is smaller than bullet radius
+                score += 1;
+                const killSound = intersects[0].object.parent.children[2];
+                killSound.play();
+                scene.remove(intersects[0].object.parent);
+                bullets[index].alive = false;
+                scene.remove(bullets[index]);
+            }
+        } catch (err) {
+            null;
+        }
+        bullets[index].position.add(bullets[index].velocity);
+    }
+    //shoot delay
+    if (canShoot > 0) canShoot -= shootDelay;
+    if (canShoot < 0) canShoot = 0;
 
-	if (canMove > 0) canMove -= moveDelay;
-	if (canMove < 0) canMove = 0;
+    if (canMove > 0) canMove -= moveDelay;
+    if (canMove < 0) canMove = 0;
 
-	//debug enemy spawn
-	if (pressedKeys[81] == true && canShoot == 0) {
-		spawnEnemyArray(3, 3, -10, 3, -10, 5); //spawnEnemyArray(col, row, innerZ, zSpace, leftX, xSpace)
-		//spawnEnemy(0, -10);
-		canShoot = 10;
-	}
-	if (pressedKeys[69] == true && canShoot == 0) {
-		enemies.forEach((element, index) => {
-			enemies[index].position.x += 1;
-		});
-	}
+    //debug enemy spawn
+    if (pressedKeys[81] == true && canShoot == 0) {
+        spawnEnemyArray(3, 3, -10, 3, -10, 5); //spawnEnemyArray(col, row, innerZ, zSpace, leftX, xSpace)
+        //spawnEnemy(0, -10);
+        canShoot = 10;
+    }
+    if (pressedKeys[69] == true && canShoot == 0) {
+        enemies.forEach((element, index) => {
+            enemies[index].position.x += 1;
+        });
+    }
 
-	//enemy moving logic
-	if (canMove == 0) {
-		if (moveHang == false) {
-			enemies.forEach((element, index) => {
-				enemies[index].position.x += moveThing;
-			});
-		}
+    //enemy moving logic
+    if (canMove == 0) {
+        if (moveHang == false) {
+            enemies.forEach((element, index) => {
+                enemies[index].position.x += moveThing;
+            });
+        }
 
-		if (moveCount == moveThreshold) {
-			moveCount = 0;
-			moveHang = false;
-			enemies.forEach((element, index) => {
-				enemies[index].position.z += 1;
-				if (enemies[0].position.z == -5) {
-					gameOver();
-				}
-			});
+        if (moveCount == moveThreshold) {
+            moveCount = 0;
+            moveHang = false;
+            enemies.forEach((element, index) => {
+                enemies[index].position.z += 1;
+                if (enemies[0].position.z == -5) {
+                    gameOver();
+                }
+            });
 
-			if (moveThing == 1) {
-				moveThing = -1;
-			} else {
-				moveThing = 1;
-			}
-		}
-		if (moveCount == moveThreshold - 1) {
-			moveHang = true;
-		}
-		moveCount++;
-		canMove = 10;
-	}
+            if (moveThing == 1) {
+                moveThing = -1;
+            } else {
+                moveThing = 1;
+            }
+        }
+        if (moveCount == moveThreshold - 1) {
+            moveHang = true;
+            console.log(moveHang);
+        }
+        moveCount++;
+        canMove = 10;
+    }
 
-	//camera follow
-	camera.position.x = player.position.x;
+    //camera follow
+    camera.position.x = player.position.x;
 
-	//check for win
-	if (score == enemies.length && score != 0) {
-		nextWave();
-	}
+    if (pressedKeys[88] == true) {
+        cameraIntro();
+    }
+    //check for win
+    if (score == enemies.length && score != 0) {
+        nextWave();
+    }
 
-	// Render
-	renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera);
 
-	// Call tick again on the next frame
-	window.requestAnimationFrame(tick);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
 
-	// Score update
-	document.getElementById("output").innerHTML = score;
+    // Score update
+    document.getElementById("output").innerHTML = score;
 };
 
 //updates key pressed array
 function onDocumentKeyDown(event) {
-	var keyCode = event.which;
-	pressedKeys[keyCode] = true;
+    var keyCode = event.which;
+    pressedKeys[keyCode] = true;
 }
 function onDocumentKeyUp(event) {
-	var keyCode = event.which;
-	pressedKeys[keyCode] = false;
+    var keyCode = event.which;
+    pressedKeys[keyCode] = false;
 }
 
 function spawnEnemy(x, z) {
-	audioLoader.load("sounds/explosionCrunch_001.ogg", function (buffer) {
-		loader.load(
-			"models/enemy.obj",
-			function (object) {
-				enemy = object;
-				enemy.scale.set(enemyScale, enemyScale, enemyScale);
-				const geometry = new THREE.BoxGeometry(10.3, 7.8, 0.5);
-				const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-				var mesh = new THREE.Mesh(geometry, material);
-				mesh.position.set(0.5, -1, 1);
-				mesh.material.visible = false;
-				enemy.add(mesh);
-				const killSound = new THREE.Audio(listener);
-				killSound.setBuffer(buffer);
-				killSound.setVolume(0.5);
-				enemy.add(killSound);
-				enemy.position.set(x, 1, z);
-				enemies.push(enemy);
-				scene.add(enemy);
-			},
-			function (xhr) {
-				console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-			},
-			function (error) {
-				console.log("An error happened");
-			}
-		);
-	});
+    audioLoader.load("sounds/explosionCrunch_001.ogg", function (buffer) {
+        loader.load(
+            "models/enemy.obj",
+            function (object) {
+                enemy = object;
+                enemy.scale.set(enemyScale, enemyScale, enemyScale);
+                const geometry = new THREE.BoxGeometry(10.3, 7.8, 0.5);
+                const material = new THREE.MeshBasicMaterial({
+                    color: 0xffff00,
+                });
+                var mesh = new THREE.Mesh(geometry, material);
+                mesh.position.set(0.5, -1, 1);
+                mesh.material.visible = false;
+                enemy.add(mesh);
+                const killSound = new THREE.Audio(listener);
+                killSound.setBuffer(buffer);
+                killSound.setVolume(0.5);
+                enemy.add(killSound);
+                enemy.position.set(x, 1, z);
+                enemies.push(enemy);
+                scene.add(enemy);
+            },
+            function (xhr) {
+                console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+            },
+            function (error) {
+                console.log("An error happened");
+            }
+        );
+    });
 }
 function spawnEnemyArray(col, row, innerZ, zSpace, leftX, xSpace) {
-	for (let i = leftX; i < col + leftX; i++) {
-		for (let j = innerZ; j > innerZ - row; j--) {
-			spawnEnemy((i - leftX) * xSpace + leftX, (j - innerZ) * zSpace + innerZ);
-		}
-	}
+    for (let i = leftX; i < col + leftX; i++) {
+        for (let j = innerZ; j > innerZ - row; j--) {
+            spawnEnemy(
+                (i - leftX) * xSpace + leftX,
+                (j - innerZ) * zSpace + innerZ
+            );
+        }
+    }
 }
 function gameOver() {
-	console.log("Gameover");
+    console.log("Gameover");
 }
 function nextWave() {
-	console.log("next wave");
+    console.log("next wave");
 }
+function cameraIntro() {
+    animateVector3(camera.position, cameraPositionTarget, {
+        duration: 5000,
+
+        easing: TWEEN.Easing.Quadratic.InOut,
+    });
+    animateVector3(camera.rotation, cameraRotationTarget, {
+        duration: 5000,
+
+        easing: TWEEN.Easing.Quadratic.InOut,
+    });
+}
+function animateVector3(vectorToAnimate, cameraPositionTarget, options) {
+    options = options || {};
+    // get targets from options or set to defaults
+    var to = cameraPositionTarget || THREE.Vector3(),
+        easing = options.easing || TWEEN.Easing.Quadratic.In,
+        duration = options.duration || 2000;
+    // create the tween
+    var tweenVector3 = new TWEEN.Tween(vectorToAnimate)
+        .to({ x: to.x, y: to.y, z: to.z }, duration)
+        .easing(easing)
+        .onUpdate(function (d) {
+            if (options.update) {
+                options.update(d);
+            }
+        })
+        .onComplete(function () {
+            if (options.callback) options.callback();
+        });
+    // start the tween
+    tweenVector3.start();
+    // return the tween in case we want to manipulate it later on
+    return tweenVector3;
+}
+/* How to use */
+
 tick();
